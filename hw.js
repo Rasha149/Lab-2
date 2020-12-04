29f8ca5273c5f6ddebe47d8c302512956136c4d8
Rasha Kahil <012o456789@gmail.com>
2020-12-04 20:42:21 +0300

    from rasha

--- hw.js
+++ hw.js
@@ -1,56 +1,56 @@
 const HW=require('./hw(1).json');
+allID();
+batters();
+topp();
+sum();
+avg();
 
+
+function allID(){
 console.log("ID");
-for(let i=0;i<HW.length;i++){
-console.log(HW[i].id);
-}
 for(let i=0;i<HW.length;i++){
     for(let z=0;z<(HW[i].batters.batter).length;z++){
     const b=HW[i].batters;
-     console.log(b.batter[z].id);
-}}
-for(let i=0;i<HW.length;i++){
-    for(let z=0;z<(HW[i].topping).length;z++){
     const t=HW[i].topping;
-     console.log(t[z].id);
-}}
+     console.log(HW[i].id,b.batter[z].id,t[z].id);
+}}}
 
 
 
+
+function batters(){
 console.log("\nThe type of batters");
 for(let i=0;i<HW.length;i++){
     for(let z=0;z<(HW[i].batters.batter).length;z++){
     const b=HW[i].batters;
      console.log(b.batter[z].type);
-}}
+}}}
 
 
-
+function topp(){
 console.log("\nThe type of Topping");
 for(let i=0;i<HW.length;i++){
     for(let z=0;z<(HW[i].topping).length;z++){
     const t=HW[i].topping;
      console.log(t[z].type);
-}}
+}}}
 
-
+function sum(){
 console.log("\nThe sum of ppu");
 var sumOfppu=0;
 for(let i=0;i<HW.length;i++){
     const element=(HW[i].ppu);
     sumOfppu+=(element);
-    console.log(sumOfppu);
-}
+   
+} console.log(sumOfppu);}
 
-
+function avg(){
 console.log("\nThe average of ppu");
 var avgOfppu=0;
 for(let i=0;i<HW.length;i++){
     const element=(HW[i].ppu);
-    avgOfppu+=(element/HW.length);
-    console.log(avgOfppu);
-}
-
+    avgOfppu+=(element/HW.length);}
+    console.log(avgOfppu);}
 
 
 
